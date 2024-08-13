c241f9b49da6740ada0243bb70e41ce3125d3ec6
Abhishree21 <abhishree.a@superbolter.com>
2024-08-13 20:38:25 +0530

    handeled image sizes

--- src/component/background.js
+++ src/component/background.js
@@ -1,15 +1,27 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useMemo } from "react";
 import { Canvas, useThree, useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three";
 import Image from "../assets/img.jpg";
 import { useSelector } from "react-redux";
 
 const BackgroundImage = () => {
-  const fixedWidth = 900; // Set the desired fixed width
-  const fixedHeight = 520; // Set the desired fixed height, or calculate based on the image aspect ratio
-  // Use TextureLoader to load the image texture asynchronously
   const image=useSelector((state)=>state.ApplicationState.img);
   const texture = useLoader(TextureLoader, image? `https://${image}`: Image);
+  const screenWidth = window.innerWidth * 0.7;
+  const screenHeight = window.innerHeight * 0.7;
+  const aspectRatio = texture.image.width / texture.image.height;
+  const [fixedWidth, fixedHeight] = useMemo(()=>{
+    if(texture.image.height > screenHeight){
+      return [screenHeight * aspectRatio, screenHeight]
+    }
+    else if(texture.image.width > screenWidth){
+      return [screenWidth, screenWidth / aspectRatio]
+    }else{
+      return [texture.image.width, texture.image.height]
+    }
+  },[screenHeight,screenWidth,aspectRatio])
+
+
 
 
   return (
@@ -23,28 +35,3 @@ const BackgroundImage = () => {
 
 
 export default BackgroundImage;
-
-
-
-// import React, { Suspense } from "react";
-// import { Canvas, useThree, useLoader } from "@react-three/fiber";
-// import { TextureLoader } from "three";
-
-// // Image URL
-// const imageUrl = "http://d21xri4ugh8s6d.cloudfront.net/atom/images/ascJhS6sWnGNVfYMH3qvxQ/774edf54-2317-404c-a712-086eeda3c9ee.png";
-
-// const BackgroundImage = () => {
-//   const { viewport } = useThree();
-
-//   // Use TextureLoader to load the image texture asynchronously
-//   const texture = useLoader(TextureLoader, imageUrl);
-
-//   return (
-//     <mesh position={[0, 0, -1]}>
-//       <planeGeometry args={[viewport.width, viewport.height]} />
-//       <meshBasicMaterial map={texture} transparent={true} opacity={0.2} />
-//     </mesh>
-//   );
-// };
-
-// export default BackgroundImage;
