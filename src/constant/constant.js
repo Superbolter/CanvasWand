4979f96daa74b203b3c3d033eea4c1d48e9260bc
Abhishree21 <abhishree.a@superbolter.com>
2024-09-25 14:59:22 +0530

    removed old code

--- src/constant/constant.js
+++
@@ -1,4 +0,0 @@
-
-export const INITIAL_BREADTH = 15; 
-export const INITIAL_HEIGHT = 300; 
-export const SNAPPING_THRESHOLD = 15;
--- src/design_system/StyledComponents/utils/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./typography";
-// export * from "./themes";
-export * from "./color";
-
