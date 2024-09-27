f57d6634dbd053cd764b5fca41caf67fa5763530
Shubham23jha <shubham742jha@gmail.com>
2024-09-27 23:00:41 +0530

    modifing snqaping

--- src/utils/snapping.js
+++ src/utils/snapping.js
@@ -28,11 +28,6 @@ export const snapToPoint = (
   snapPoint,
   linePlacementMode
 ) => {
-  // console.log("upperPoints called with: ", {
-  //   snapPoint,
-  //   snapActive,
-  //   linePlacementMode,
-  // });
   if (!snapActive) {
     return point;
   }
@@ -59,40 +54,105 @@ export const snapToPoint = (
   }
 
   for (let line of storeLines) {
+
+    const upperP0 = upperPoints(
+      line.points[0],
+      line.points[1],
+      line.width,
+      line.points[0],
+      factor,
+      measured
+    );
+
+    const upperP1 = upperPoints(
+      line.points[0],
+      line.points[1],
+      line.width,
+      line.points[1],
+      factor,
+      measured
+    );
     if (
-      line.points[0].distanceTo(point) < (snapActive ? SNAPPING_THRESHOLD : 0)
+      line.points[0].distanceTo(point) < (snapActive ? SNAPPING_THRESHOLD : 0)||
+      upperP0.distanceTo(point) < (snapActive ? SNAPPING_THRESHOLD : 0)
     ) {
       let closestPoint = line.points[0];
+      let upperP = upperP0;
+      const upperDistance = upperP.distanceTo(point);
+      const belowDistance = closestPoint.distanceTo(point);
       if(snapPoint === "upper" && linePlacementMode === "below"){
-          closestPoint = upperPoints(
-            line.points[0],
-            line.points[1],
-            line.width,
-            line.points[0],
-            factor,
-            measured
-          );
-      }
-      return closestPoint;
-    }
-    if (
-      line.points[1].distanceTo(point) < (snapActive ? SNAPPING_THRESHOLD : 0)
-    ) {
-      let closestPoint = line.points[1];
-      if(snapPoint === "upper" && linePlacementMode === "below"){
-          closestPoint = upperPoints(
-            line.points[0],
-            line.points[1],
-            line.width,
-            line.points[1],
-            factor,
-            measured
-          );
+          if(upperDistance >= belowDistance ){
+            closestPoint = upperP;
+          }
       }
       return closestPoint;
     }
 
+
+    if (
+      line.points[1].distanceTo(point) < (snapActive ? SNAPPING_THRESHOLD : 0)||
+      upperP1.distanceTo(point) < (snapActive ? SNAPPING_THRESHOLD : 0)
+    ) {
+      let closestPoint = line.points[1];
+      let upperP = upperP1;
+
+      const upperDistance = upperP.distanceTo(point);
+      const belowDistance = closestPoint.distanceTo(point);
+      if(snapPoint === "upper" && linePlacementMode === "below"){
+        if(upperDistance >= belowDistance ){
+          closestPoint = upperP;
+        }
+      }
+      return closestPoint;
+    }
+    
+
     // Check for snapping to the line segment itself
+    // let closestPointOnLine = closestPointOnSegment(
+    //   line.points[0],
+    //   line.points[1],
+    //   point
+    // );
+    // let closestUpperPointOnLine = closestPointOnSegment(
+    //   upperPoints(line.points[0], line.points[1], line.width, line.points[0], factor, measured),
+    //   upperPoints(line.points[0], line.points[1], line.width, line.points[1], factor, measured),
+    //   point
+    // );
+
+    // const closestPointDistance = closestPointOnLine.distanceTo(point);
+    // const closestUpperDistance = closestUpperPointOnLine.distanceTo(point);
+    // if (closestPointDistance < SNAPPING_THRESHOLD || closestUpperDistance < SNAPPING_THRESHOLD) {
+
+    //   if (snapPoint === "upper" && linePlacementMode === "below") {
+    //    if((closestPointDistance<=closestUpperDistance) ){
+    //     closestPointOnLine = upperPoints(
+    //       line.points[0],
+    //       line.points[1],
+    //       line.width,
+    //       closestPointOnLine,
+    //       factor,
+    //       measured
+    //     );
+    //    }
+    //    else if((closestPointDistance>closestUpperDistance)||closestUpperDistance < SNAPPING_THRESHOLD){
+    //     if (snapPoint === "upper" && linePlacementMode === "below") {
+    //     closestPointOnLine = upperPoints(
+    //       line.points[0],
+    //       line.points[1],
+    //       line.width,
+    //       closestUpperPointOnLine,
+    //       factor,
+    //       measured
+    //     );
+    //    }
+    //   }
+
+    //   }
+
+      
+
+    //   return closestPointOnLine;
+    // }
     let closestPointOnLine = closestPointOnSegment(
       line.points[0],
       line.points[1],
@@ -108,9 +168,7 @@ export const snapToPoint = (
           factor,
           measured
         );
-
       }
-
       return closestPointOnLine;
     }
   }
