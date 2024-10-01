4cef0a355103a608c50b3ec678f123f18cb8b4fc
Abhishree21 <abhishree.a@superbolter.com>
2024-10-01 12:45:08 +0530

    optimised draw tool

--- src/component/Geometry/BoxGeometry.js
+++ src/component/Geometry/BoxGeometry.js
@@ -2,93 +2,17 @@ import React, { useEffect, useMemo, useState } from "react";
 import { Canvas, extend } from "@react-three/fiber";
 import {
   Vector3,
-  Shape,
   ShapeGeometry,
-  MeshBasicMaterial,
-  TextureLoader,
 } from "three";
 import { useDispatch, useSelector } from "react-redux";
 import { Text } from "@react-three/drei";
 import convert from "convert-units";
-import Window from "../../assets/Window.png";
-import Door from "../../assets/Door.png";
-import Railing from "../../assets/Railing.png";
-import New from "../../assets/New.png";
-import Wall from "../../assets/Walll.png";
-import HiddenWall from "../../assets/hiddenwall.png";
-import Opening from "../../assets/Opening.png";
-import newWall from "../../assets/newWall.png";
-import newWall2 from "../../assets/newWall2.png";
-import { setStoreBoxes } from "../../Actions/ApplicationStateAction";
-import cursor from "../../assets/Default.png";
 import usePoints from "../../hooks/usePoints";
 import { setHiglightPoint } from "../../Actions/DrawingActions";
 import { useDrawing } from "../../hooks/useDrawing";
+import useTexture from "../../hooks/useTexture";
 // Extend the R3F renderer with ShapeGeometry
-extend({ ShapeGeometry });
-
-const calculateSlope = (angle) => {
-  // Handle vertical lines (angle of π/2 or -π/2)
-  if (angle === Math.PI / 2 || angle === -Math.PI / 2) {
-    return Infinity;
-  }
-  return Math.tan(angle);
-};
-
-// Function to calculate the y-intercept given a point and slope
-const calculateIntercept = (point, slope) => {
-  if (slope === Infinity) {
-    // For vertical lines, the intercept can be considered the x-coordinate
-    return point.x;
-  }
-  return point.y - slope * point.x;
-};
-
-// Function to calculate the intersection point of two lines
-const calculateIntersection = (point1, angle1, point2, angle2) => {
-  const slope1 = calculateSlope(angle1);
-  const intercept1 = calculateIntercept(point1, slope1);
-
-  const slope2 = calculateSlope(angle2);
-  const intercept2 = calculateIntercept(point2, slope2);
-
-  // If both lines are vertical and have the same x-coordinate
-  if (slope1 === Infinity && slope2 === Infinity && intercept1 === intercept2) {
-    return new Vector3(point1.x, point1.y, 0);
-  }
-
-  // If both lines are vertical and have different x-coordinates
-  if (slope1 === Infinity && slope2 === Infinity) {
-    return null;
-  }
-
-  // If one line is vertical
-  if (slope1 === Infinity) {
-    const x = intercept1;
-    const y = slope2 * x + intercept2;
-    return new Vector3(x, y, 0);
-  }
-
-  // If the other line is vertical
-  if (slope2 === Infinity) {
-    const x = intercept2;
-    const y = slope1 * x + intercept1;
-    return new Vector3(x, y, 0);
-  }
-
-  // If the slopes are the same, the lines are parallel
-  if (slope1 === slope2) {
-    return null;
-  }
-
-  // Calculate the x-coordinate of the intersection
-  const x = (intercept2 - intercept1) / (slope1 - slope2);
-
-  // Calculate the y-coordinate of the intersection using one of the line equations
-  const y = slope1 * x + intercept1;
-
-  return new Vector3(x, y, 0);
-};
+// extend({ ShapeGeometry });
 
 const BoxGeometry = ({
   start,
@@ -96,9 +20,7 @@ const BoxGeometry = ({
   dimension,
   isSelected,
   typeId,
-  onClick,
   showDimension = false,
-  isCustomised,
   opacity = 0.5,
   distance = null,
   lineId = null
@@ -106,14 +28,11 @@ const BoxGeometry = ({
   const dispatch = useDispatch();
   const [hovered, setHovered] = useState(false);
 
-  const { measured, roomSelect, newline, linePlacementMode, userWidth } =
+  const { measured, linePlacementMode } =
     useSelector((state) => state.drawing);
 
   const {
-    storeLines,
     factor,
-    storeBoxes,
-    points,
     designStep,
     selectionMode,
     activeRoomIndex,
@@ -123,238 +42,84 @@ const BoxGeometry = ({
     (state) => state.Drawing
   );
 
-  const {handleLineClick} = useDrawing();
+  const { handleLineClick } = useDrawing();
 
   const { decimalToFeetInches } = usePoints();
 
-  const textureLoader = useMemo(() => new TextureLoader(), []);
-  const windowTexture = useMemo(
-    () => textureLoader.load(Window), // Replace with the path to your window image
-    [textureLoader]
-  );
-  const wallTexture = useMemo(
-    () => textureLoader.load(newWall2), // Replace with the path to your window image
-    [textureLoader]
-  );
-  const railingTexture = useMemo(
-    () => textureLoader.load(Railing), // Replace with the path to your window image
-    [textureLoader]
-  );
-  const doorTexture = useMemo(
-    () => textureLoader.load(Door), // Replace with the path to your window image
-    [textureLoader]
-  );
-  const newTexture = useMemo(
-    () => textureLoader.load(New), // Replace with the path to your window image
-    [textureLoader]
-  );
-  const hiddenWallTexture = useMemo(
-    () => textureLoader.load(HiddenWall),
-    [textureLoader]
-  );
+  const {
+    wallTexture,
+    doorTexture,
+    windowTexture,
+    railingTexture,
+    hiddenWallTexture,
+    selectedTexture,
+    hoveredTexture,
+  } = useTexture();
 
-  const selectedTexture = useMemo(
-    () => textureLoader.load(Opening),
-    [textureLoader]
-  );
+  const memoisedLine = useMemo(() => {
+    const length = start.distanceTo(end);
+    const width = convert(dimension.width * factor[1])
+      .from("mm")
+      .to(measured);
+    // const width = isCustomised ? (1/factor[0]) * dimension.width :(1/factor[0]) * userWidth;
 
-  const length = start.distanceTo(end);
-  const width = convert(dimension.width * factor[1])
-    .from("mm")
-    .to(measured);
-  // const width = isCustomised ? (1/factor[0]) * dimension.width :(1/factor[0]) * userWidth;
+    const midpoint = new Vector3().addVectors(start, end).multiplyScalar(0.5);
+    const angle = Math.atan2(end.y - start.y, end.x - start.x);
+    let direction = new Vector3().subVectors(end, start).normalize();
 
-  const midpoint = new Vector3().addVectors(start, end).multiplyScalar(0.5);
-  const angle = Math.atan2(end.y - start.y, end.x - start.x);
-  let direction = new Vector3().subVectors(end, start).normalize();
-
-  // Calculate the perpendicular vector
-  let perpVector = new Vector3(-direction.y, direction.x, 0).multiplyScalar(
-    width / 2
-  );
-
-  // Calculate the four corners
-  let upperLeft = new Vector3().addVectors(start, perpVector);
-  let lowerLeft = new Vector3().subVectors(start, perpVector);
-  let upperRight = new Vector3().addVectors(end, perpVector);
-  let lowerRight = new Vector3().subVectors(end, perpVector);
-
-  let p1, p2, p3, p4, mid1, mid2, prevAngle;
-  if (linePlacementMode === "below") {
-    // Calculate the offset to shift the midpoint by half the width
-    const offset = new Vector3(
-      (-Math.sin(angle) * width) / 2,
-      (Math.cos(angle) * width) / 2,
-      0
-    );
-    const adjustedMidpoint = midpoint.add(offset);
-    //console.log("hello adjustmid :", adjustedMidpoint);
-    perpVector = new Vector3(-direction.y, direction.x, 0).multiplyScalar(
-      width
+    // Calculate the perpendicular vector
+    let perpVector = new Vector3(-direction.y, direction.x, 0).multiplyScalar(
+      width / 2
     );
 
     // Calculate the four corners
-    upperLeft = new Vector3().addVectors(start, perpVector);
-    lowerLeft = new Vector3().subVectors(start, perpVector);
-    upperRight = new Vector3().addVectors(end, perpVector);
-    lowerRight = new Vector3().subVectors(end, perpVector);
+    let upperLeft = new Vector3().addVectors(start, perpVector);
+    let upperRight = new Vector3().addVectors(end, perpVector);
 
-    let idx = storeLines.findIndex(
-      (line) => line.points[0] === start && line.points[1] === end
+    // Calculate the text position slightly above the midpoint
+    const textPosition = new Vector3(
+      midpoint.x - 20 * Math.sin(angle),
+      midpoint.y + 20 * Math.cos(angle),
+      0.1 // Ensure it's above the box geometry
     );
-    //console.log("index",idx)
 
-    if (idx >= 1) {
-      //console.log("shubham")
+    // Calculate the rotation angle for the text based on the line angle
+    const textRotation = angle;
 
-      const line = storeLines[idx - 1];
-      //console.log("hello line :", line)
-      const prevWidth = convert(line.width * factor[1])
-        .from("mm")
-        .to(measured);
-      const prevMid = new Vector3()
-        .addVectors(line.points[0], line.points[1])
-        .multiplyScalar(0.5);
-      prevAngle = Math.atan2(
-        line.points[1].y - line.points[0].y,
-        line.points[1].x - line.points[0].x
-      );
-      const prevoffset = new Vector3(
-        (-Math.sin(prevAngle) * width) / 2,
-        (Math.cos(prevAngle) * prevWidth) / 2,
-        0
-      );
-      const prevAdjustedMidpoint = prevMid.add(prevoffset);
+    // State to manage hover state
 
-      //console.log("prevAdjustedMidpoint:", prevAdjustedMidpoint);
+    // State to manage the current texture
 
-      if (line.points[1] === start) {
-        if (line.points[0].x === line.points[1].x) {
-          mid1 = new Vector3(prevAdjustedMidpoint.x, line.points[1].y, 0);
-          p4 = line.points[1];
-          p1 = new Vector3(2 * mid1.x - p4.x, 2 * mid1.y - p4.y, 0);
-        } else if (line.points[0].y === line.points[1].y) {
-          mid1 = new Vector3(line.points[1].x, prevAdjustedMidpoint.y, 0);
-          p4 = line.points[1];
-          p1 = new Vector3(2 * mid1.x - p4.x, 2 * mid1.y - p4.y, 0);
-        } else {
-          p4 = line.points[1];
-          mid1 = p4.clone().add(prevoffset);
-          p1 = mid1.clone().add(prevoffset);
-        }
-      }
+    var feetLength = 0;
+    if (measured === "ft") {
+      feetLength = decimalToFeetInches(length * factor[0]);
     }
-
-    if (start.x === end.x) {
-      mid2 = new Vector3(adjustedMidpoint.x, start.y, 0);
-
-      p4 = start;
-      p2 = new Vector3(2 * mid2.x - p4.x, 2 * mid2.y - p4.y, 0);
-    } else if (start.y === end.y) {
-      mid2 = new Vector3(start.x, adjustedMidpoint.y, 0);
-      p4 = start;
-      p2 = new Vector3(2 * mid2.x - p4.x, 2 * mid2.y - p4.y, 0);
-    } else {
-      p4 = start;
-      mid2 = p4.clone().add(offset);
-      p2 = mid2.clone().add(offset);
-    }
-
-    if (mid1 && mid2 && !newline) {
-      if (mid1 !== mid2 && p1 !== p2) {
-        
-        p3 = calculateIntersection(p1, prevAngle, p2, angle);
-
-        
-      }
-    }
-
-    // let p1 = new Vector3(start.x, start.y+width, 0);
-    // let p2 = new Vector3(start.x+ width, start.y, 0)
-    // let p3 = new Vector3(start.x+ width, start.y+width, 0);
-    // let p4 = start;
-
-    //console.log("mid1 and mid2 :", mid1, mid2);
-    // console.log('p1:',p1);
-    // console.log(p2);
-    // console.log(p3);
-    // console.log(p4);
-  }
-  useEffect(() => {
-    if (mid1 && mid2 && mid1 !== mid2) {
+    
+    const getTexture = () => {
+      if (isSelected) return selectedTexture;
+      if (hovered && selectionMode && designStep === 2) return hoveredTexture;
       if (
-        Math.abs(p1?.x) < 1000 &&
-        Math.abs(p1?.y) < 1000 &&
-        Math.abs(p2?.x) < 1000 &&
-        Math.abs(p2?.y) < 1000 &&
-        Math.abs(p3?.x) < 1000 &&
-        Math.abs(p3?.y) < 1000 &&
-        Math.abs(p4?.x) < 1000 &&
-        Math.abs(p4?.y) < 1000
-      ) {
-        // console.log("intersection point found between mid1 and mid2",p1,p2,p3,p4)
-        const data = { p1, p2, p3, p4 };
-        const boxes = [...storeBoxes];
-        if (!boxes.find((box) => box.p1.x === p1.x && box.p1.y === p1.y)) {
-          boxes.push(data);
-          dispatch(setStoreBoxes(boxes));
-        }
-      }
-    } else {
-      const has = (p) => {
-        return points.some((point) => point.x === p.x && point.y === p.y);
-      };
-      const filteredBoxes = storeBoxes.filter((box) => {
-        const { p1, p2, p3, p4 } = box;
-        return has(p1) || has(p2) || has(p3) || has(p4);
-      });
-      dispatch(setStoreBoxes(filteredBoxes));
-    }
-  }, [points]);
+        hovered &&
+        selectionMode &&
+        designStep === 3 &&
+        activeRoomIndex !== -1 &&
+        activeRoomButton !== "divide"
+      )
+        return hoveredTexture;
+      if (typeId === 1) return wallTexture;
+      if (typeId === 2) return doorTexture;
+      if (typeId === 3) return windowTexture;
+      if (typeId === 4) return railingTexture;
+      if (typeId === 5) return hiddenWallTexture;
+      return "";
+    };
 
-  // Calculate the text position slightly above the midpoint
-  const textPosition = new Vector3(
-    midpoint.x - 20 * Math.sin(angle),
-    midpoint.y + 20 * Math.cos(angle),
-    0.1 // Ensure it's above the box geometry
-  );
-
-  // Calculate the rotation angle for the text based on the line angle
-  const textRotation = angle;
-
-  // State to manage hover state
-
-  // State to manage the current texture
-  const getTexture = () => {
-    if (isSelected) return wallTexture;
-    if (hovered && selectionMode && designStep === 2) return selectedTexture;
-    if (
-      hovered &&
-      selectionMode &&
-      designStep === 3 &&
-      activeRoomIndex !== -1 &&
-      activeRoomButton !== "divide"
-    )
-      return selectedTexture;
-    if (typeId === 1) return newTexture;
-    if (typeId === 2) return doorTexture;
-    if (typeId === 3) return windowTexture;
-    if (typeId === 4) return railingTexture;
-    if (typeId === 5) return hiddenWallTexture;
-    return "";
-  };
-  var feetLength = 0;
-  if (measured === "ft") {
-    feetLength = decimalToFeetInches(length * factor[0]);
-  }
-
-  const memoisedLine = useMemo(() => {
     return (
       <>
         <mesh
           position={midpoint}
           rotation={[0, 0, angle]}
-          onClick={(e) => handleLineClick(e,lineId)}
+          onClick={(e) => handleLineClick(e, lineId)}
           onPointerOver={() => {
             // if (activeRoomIndex !== -1) {
             //   document.getElementsByClassName(
@@ -383,7 +148,7 @@ const BoxGeometry = ({
         <mesh
           position={start}
           onPointerOver={() => {
-            if (designStep === 2) {
+            if (designStep === 2 && lineId) {
               dispatch(setHiglightPoint(start));
             }
           }}
@@ -397,14 +162,14 @@ const BoxGeometry = ({
               start === higlightPoint
                 ? "green"
                 : typeId === 2
-                ? "brown"
-                : typeId === 3
-                ? "skyblue"
-                : typeId === 4
-                ? "violet"
-                : typeId === 4
-                ? "#E6AB4A"
-                : "black"
+                  ? "brown"
+                  : typeId === 3
+                    ? "skyblue"
+                    : typeId === 4
+                      ? "violet"
+                      : typeId === 4
+                        ? "#E6AB4A"
+                        : "black"
             }
             transparent={true}
             opacity={opacity}
@@ -414,7 +179,7 @@ const BoxGeometry = ({
         {(seeDimensions || showDimension) && typeId !== 5 && (
           <Text
             position={textPosition}
-            rotation={[0, 0, textRotation < 3 ? textRotation: 0]}
+            rotation={[0, 0, textRotation < 3 ? textRotation : 0]}
             fontSize={9}
             color="black"
             anchorX="center"
@@ -423,15 +188,15 @@ const BoxGeometry = ({
             {distance
               ? `${length.toFixed(2)}`
               : measured === "ft"
-              ? `${feetLength.feet}'${feetLength.inches} ${measured}`
-              : `${(length * factor[0]).toFixed(2)} ${measured}`}
+                ? `${feetLength.feet}'${feetLength.inches} ${measured}`
+                : `${(length * factor[0]).toFixed(2)} ${measured}`}
           </Text>
         )}
 
         <mesh
           position={end}
           onPointerOver={() => {
-            if (designStep === 2) {
+            if (designStep === 2 && lineId) {
               dispatch(setHiglightPoint(end));
             }
           }}
@@ -445,14 +210,14 @@ const BoxGeometry = ({
               end === higlightPoint
                 ? "green"
                 : typeId === 2
-                ? "brown"
-                : typeId === 3
-                ? "skyblue"
-                : typeId === 4
-                ? "violet"
-                : typeId === 4
-                ? "#E6AB4A"
-                : "black"
+                  ? "brown"
+                  : typeId === 3
+                    ? "skyblue"
+                    : typeId === 4
+                      ? "violet"
+                      : typeId === 4
+                        ? "#E6AB4A"
+                        : "black"
             }
             transparent={true}
             opacity={opacity}
@@ -466,19 +231,19 @@ const BoxGeometry = ({
                 typeId === 2
                   ? "brown"
                   : typeId === 3
-                  ? "skyblue"
-                  : typeId === 4
-                  ? "violet"
-                  : typeId === 4
-                  ? "#E6AB4A"
-                  : "black"
+                    ? "skyblue"
+                    : typeId === 4
+                      ? "violet"
+                      : typeId === 4
+                        ? "#E6AB4A"
+                        : "black"
               }
               transparent={true}
               opacity={opacity}
             />
           </mesh>
         )}
-        
+
         {linePlacementMode !== "midpoint" && (
           <mesh position={upperRight}>
             <sphereGeometry args={[4, 16, 16]} />
@@ -487,22 +252,22 @@ const BoxGeometry = ({
                 typeId === 2
                   ? "brown"
                   : typeId === 3
-                  ? "skyblue"
-                  : typeId === 4
-                  ? "violet"
-                  : typeId === 4
-                  ? "#E6AB4A"
-                  : "black"
+                    ? "skyblue"
+                    : typeId === 4
+                      ? "violet"
+                      : typeId === 4
+                        ? "#E6AB4A"
+                        : "black"
               }
               transparent={true}
               opacity={opacity}
             />
           </mesh>
         )}
-        
+
       </>
     );
-  }, [start, end, isSelected, hovered, activeRoomIndex, typeId, higlightPoint, seeDimensions, showDimension, linePlacementMode, distance, measured, factor, opacity, designStep]);
+  }, [start, end, isSelected, hovered, activeRoomIndex, typeId, higlightPoint === start, higlightPoint === end, seeDimensions, showDimension, linePlacementMode, distance, measured, factor, opacity, designStep]);
 
   if (!start || !end) return null;
 
